a291b3166c2aeaf47f11fff74c4edd510483bf8f
S.M. HUANG <ad19790105@gmail.com>
2020-04-09 23:30:34 +0800

    Updates

---
+++ precache-manifest.b96d17579d54dc3efd8d7b6797a392e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569ecfd7884385ca1457785d9fd878e0",
+    "url": "/react-gh-pages-demo/index.html"
+  },
+  {
+    "revision": "3f2877c94b93256bdef2",
+    "url": "/react-gh-pages-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "393bff4192ed6db6eb39",
+    "url": "/react-gh-pages-demo/static/js/2.dd15c8cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages-demo/static/js/2.dd15c8cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f2877c94b93256bdef2",
+    "url": "/react-gh-pages-demo/static/js/main.7d55747a.chunk.js"
+  },
+  {
+    "revision": "1ebb06e08f49fef3fb0d",
+    "url": "/react-gh-pages-demo/static/js/runtime-main.5eb0c27c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
